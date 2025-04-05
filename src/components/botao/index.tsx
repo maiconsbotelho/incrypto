650e54ca02563d9b097c3eea184a6f7b6046fc6d
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-04-04 22:17:38 -0300

    refactor:adicionado efeito visual de clique no botão

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 interface BotaoProps {
   onClick: () => void;
   children: React.ReactNode;
 }
 
 export function Botao({ onClick, children }: BotaoProps) {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+    onClick();
+    setTimeout(() => setClicked(false), 200); // Reseta o estado após 200ms
+  };
+
   return (
     <button
-      onClick={onClick}
-      className="mt-4 px-6 py-2 bg-gradient-to-r from-[#00d2ff] via-[#3a47d5] to-[#7e1adb] text-white font-bold rounded-full shadow-lg hover:scale-105 transition-transform duration-300"
+      onClick={handleClick}
+      className={`mt-4 px-6 py-2 bg-gradient-to-r from-[#00d2ff] via-[#3a47d5] to-[#7e1adb] text-white font-bold rounded-full shadow-lg transition-transform duration-300 ${
+        clicked ? "scale-95" : "hover:scale-105"
+      }`}
     >
       {children}
     </button>
--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <div className="flex flex-col  w-[200px] mx-auto items-center justify-center">
       <Image src={LogoSVG} alt="Logo" width={80} height={80} />
-      <h1 className="text-2xl font-bold  text-white">Incrypto</h1>
+      <h1 className="text-2xl mt-4 font-bold  text-white">Incrypto</h1>
     </div>
   );
 }
