9522b9ba65d7e55846cb297c3f2838e3f96d7463
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-04-04 22:41:05 -0300

    refactor: adicionado efeito de digitação no display

--- src/components/display/index.tsx
+++ src/components/display/index.tsx
@@ -1,15 +1,35 @@
-import { div } from "three/tsl";
+import { useState, useEffect } from "react";
 
 interface DisplayProps {
   result: string;
 }
 
 export function Display({ result }: DisplayProps) {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    // Resetamos o texto antes de começar a digitar novamente
+    setDisplayedText("");
+
+    let index = 0;
+    const typingSpeed = 80;
+
+    const interval = setInterval(() => {
+      if (index < result.length) {
+        const nextChar = result.charAt(index);
+        setDisplayedText((prev) => prev + nextChar);
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, typingSpeed);
+
+    return () => clearInterval(interval);
+  }, [result]);
+
   return (
-    <div className="w-[90%]">
-      <p className="mt-8  p-4 bg-black/40 rounded border border-white max-w-md lg:max-w-full break-words text-center">
-        {result}
-      </p>
+    <div className="w-[90%] mt-8 p-4 bg-black/40 rounded border border-white max-w-md lg:max-w-full break-words text-center">
+      <p className="text-white whitespace-pre-wrap">{displayedText}</p>
     </div>
   );
 }
